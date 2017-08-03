e3541ceb8926a8af9a2992b1843eb2f4f8e9ab15
bobbidigi <bobbidigi34@gmail.com>
2017-08-03 18:49:53 -0400

    added geolocation

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array) {
 }
 
 var storyText =
-  "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+  " :inserta:, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 
 var insertX = ["Willy the Goblin"+"<img src='http://qnimate.com/wp-content/uploads/2014/03/images2.jpg'>", "Big Daddy", "Father Christmas"];
@@ -20,11 +20,12 @@ randomize.addEventListener('click', result);
 
 function result() {
   var newStory = storyText;
+//  var aItem = randomValueFromArray(insertA);
   var xItem = randomValueFromArray(insertX);
   var yItem = randomValueFromArray(insertY);
   var zItem = randomValueFromArray(insertZ);
 
-  var newStory = newStory.replace(':insertx:', xItem);
+  var newStory = newStory.replace(':inserta:', AItem);
   var newStory = newStory.replace(':insertx:', xItem);
   var newStory = newStory.replace(':inserty:', yItem);
   var newStory = newStory.replace(':insertz:', zItem);
@@ -48,3 +49,47 @@ function result() {
   story.innerHTML = newStory;
   story.style.visibility = 'visible';
 }
+
+
+//geolocation//geolocation//
+
+  if ("geolocation" in navigator) {
+  $('.js-geolocation').show(); 
+} else {
+  $('.js-geolocation').hide();
+}
+
+ //Where in the world are you? 
+$('.randomize').on('click', function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+  });
+});
+
+/* 
+* Test Locations
+* Austin lat/long: 30.2676,-97.74298
+* Austin WOEID: 2357536
+*/
+$(document).ready(function() {
+  loadWeather('Seattle',''); //@params location, woeid
+});
+
+function loadWeather(location) {
+  $.simpleWeather({
+    location: location,
+    success: function(weather) {
+       html = '<h2><i class="'+weather.code+'"></i> '+'</h2>';
+      
+      AItem = '<li class="currently">It was a '+weather.currently+' day in '+weather.city+', '+weather.region+'</li></ul>';  
+      
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
+
+
+
